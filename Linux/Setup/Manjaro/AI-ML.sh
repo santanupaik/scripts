ec80dc8d36adb12652fe873263a1fc1c02544e34
ITNerd1 <libereco.95@proton.me>
2023-12-31 18:34:18 +0530

    Modified file to better suit users with limited resources.

--- Linux/Setup/Manjaro/AI-ML.sh
+++ Linux/Setup/Manjaro/AI-ML.sh
@@ -2,20 +2,18 @@
 [ "$UID" -eq 0 ] || exec sudo "$0" "$@"
 echo
 echo "This script will install the necessary packages for basic python development on Manjaro."
-sudo pacman-mirrors -f 2
-sudo pacman -Sy --noconfirm python python-pip python-opencv python-matplotlib python-openai python-pandas python-openpyxl jupyter-notebook onlyoffice
+sudo pacman-mirrors -c india
+sudo pacman -Sy --noconfirm python-opencv python-matplotlib python-openai python-pandas python-openpyxl python-numpy python-seaborn
 echo
 echo "Packages installed:"
-echo "┬──────────────────"
-echo "├─> onlyoffice"
-echo "├─> jupyter notebook"
-echo "└┬> python"
-echo " ├─> pip"
+echo "─┬─────────────────"
 echo " ├─> opencv"
 echo " ├─> matplotlib"
 echo " ├─> openai"
 echo " ├─> pandas"
-echo " └─> openpyxl"
+echo " ├─> openpyxl"
+echo " ├─> numpy"
+echo " └─> seaborn"
 echo
 echo "Setup Script by ITNerd1."
 echo "You are now ready for developing in python :)"
