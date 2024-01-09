66f7133e3f1c74ea931245580a2a8f6e6b38e431
ITNerd1 <libereco.95@proton.me>
2024-01-10 05:08:26 +0530

    Cleaned up code and made it more user-friendly

--- Linux/Server/https-setup.sh
+++ Linux/Server/https-setup.sh
@@ -1,32 +1,56 @@
 #!/bin/bash
+
+# Check for root user
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root !!"
   exit
 fi
+
+# Intro Message
+printf "\nThis script will setup https with Nginx \nFor starters: Set your Email Address & Domain Name (example.com) WITHOUT any Sub-domain (www.)\n"
+
+# Get Email & Domain name from user
+printf "\nEnter Your Email Address: " && read EMAIL
 echo
-echo "This script will setup Nginx & UFW"
-echo "For starters: Set your Email Address & Domain Name (example.com) WITHOUT any Sub-domain (www.)"
+printf "\nEnter Your Domain Name: " && read DOMAIN
 echo
-printf "Enter Your Email Address: "
-read EMAIL
-echo
-printf "Enter Your Domain Name: "
-read DOMAIN
-echo
+
+# Set the Present Working Directory
 DIR=$(pwd)
+
+# Install acme.sh client
+printf "\nInstalling acme.sh client...\n"
 curl https://get.acme.sh | sh -s email=$EMAIL
+
+# Creating required directories
+printf "\nCreating the required Directories...\n"
 mkdir -pv /var/log/nginx/$DOMAIN
 mkdir -pv /etc/nginx/ssl/$DOMAIN
+
+# Generating dhparams for ssl
+printf "\nGenerating dhparams for SSL...\n"
 cd /etc/nginx/ssl/$DOMAIN
 openssl dhparam -out dhparams.pem -dsaparam 4096
 cd $DIR
+
+# Issue an ssl/tls Certificate from LetsEncrypt
+printf "\nIssuing TLS Certificate...\n"
 bash /root/.acme.sh/acme.sh \
 --issue -d itnerd.dev \
 -d www.itnerd.dev \
 -w /var/www/itnerd.dev \
 -k 4096 --server letsencrypt
+
+# Copy https config to required place
+printf "\nCopying https config...\n"
 < https.conf tee /etc/nginx/sites-available/$DOMAIN >/dev/null
+
+# Verify Nginx config
+printf "Verifying Nginx Config..."
 nginx -t
+
+# Install the ssl/tls certificate in Nginx & reload it
+printf "\nInstalling the TLS Certificate...\n"
 bash /root/.acme.sh/acme.sh \
 --install-cert -d $DOMAIN \
 --cert-file /etc/nginx/ssl/$DOMAIN/cert \
--- Linux/Server/nginx-setup.sh
+++ Linux/Server/nginx-setup.sh
@@ -1,34 +1,70 @@
+#!/bin/bash
+
+# Intro Message
+printf "\nThis script will setup Nginx & UFW \nFor starters, set your domain name (example.com) WITHOUT any Sub-domain (www.)\n"
+
+# Get Domain Name from User
+printf "\nEnter Your Domain Name: " && read DOMAIN
 echo
-echo "This script will setup Nginx & UFW"
-echo "For starters, set your domain name (example.com) WITHOUT any Sub-domain (www.)"
-echo
-printf "Enter Your Domain Name: "
-read DOMAIN
-echo
+
+# Check & grant sudo priviledges
+[ "$UID" -eq 0 ] || exec sudo "$0" "$@"
+
+# Update & Install required Packages
+printf "\nUpdating & Installing Required Packages...\n"
 sudo apt-get update
 sudo apt install git bc wget vim htop openssl curl socat rsync ufw nginx
+
+# Start required Services
+printf "\nStarting Systemd Services...\n"
 sudo systemctl enable --now nginx
 sudo systemctl enable --now ssh
 sudo systemctl enable --now rsync
 sudo systemctl enable --now ufw
+
+# Setup UFW to allow ssh & Nginx
+printf "\nSetting up UFW...\n"
 sudo ufw allow ssh
 sudo ufw allow 'Nginx Full'
 sudo ufw enable
+
+# Create required directories
+printf "\nCreating Required Directories...\n"
 sudo mkdir -p /var/www/$DOMAIN
 sudo mkdir -p /etc/nginx/includes
 sudo mkdir -p /var/www/$DOMAIN/.well-known/acme-challenge/
 sudo mkdir -p /root/Server
+
+# Move https related files to root
+printf "\nMoving https related files to root...\n"
 sudo mv https-setup.sh /root/Server/
 sudo mv https.conf /root/Server/
+
+# Set appropriate directory permissions
+printf "\nSetting Necessary Folder Permissions...\n"
 sudo chown -R $USER:$USER /var/www/$DOMAIN
 sudo chmod -R 755 /var/www/$DOMAIN
 sudo chown -R www-data:www-data /var/www/$DOMAIN/.well-known/acme-challenge/
 sudo chmod -R 0555 /var/www/$DOMAIN/.well-known/acme-challenge/
+
+# Copy required configuration to their appropriate places
+printf "\nCopying required config files...\n"
 < website.conf tee /var/www/$DOMAIN/index.html >/dev/null
 < letsencrypt-webroot.conf sudo tee /etc/nginx/includes/letsencrypt-webroot >/dev/null
 < http.conf sudo tee /etc/nginx/sites-available/$DOMAIN >/dev/null
+
+# Symlink nginx config
+printf "\nFinalizing Nginx Server Setup...\n"
 sudo ln -s /etc/nginx/sites-available/$DOMAIN /etc/nginx/sites-enabled/
+
+# Set nginx hash bucket size
 sudo sed -i 's/# server_names_hash_bucket_size/server_names_hash_bucket_size/g' /etc/nginx/nginx.conf
+
+# Check nginx config & restart it
+printf "\nVerifying Nginx config...\n"
 sudo nginx -t
+printf "\nRestarting Nginx...\n"
 sudo systemctl restart nginx
 
+# Inform the user about running the https script
+printf "\nChange to root user and run https-setup.sh stored inside the Server folder to get https support and complete the Setup.\n"
