70c19796f242339c8c29902acee769105c522554
santanupaik <admin@itnerd.dev>
2024-07-21 21:25:28 +0530

    Simplified and improved syntax

--- Android/romdev.sh
+++ Android/romdev.sh
@@ -1,29 +1,20 @@
 #!/bin/bash
 
 wget https://dl.google.com/android/repository/platform-tools-latest-linux.zip
-unzip platform-tools-latest-linux.zip -d ~
+unzip -j platform-tools-latest-linux.zip -d ~/.local/bin
 
-curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
-chmod a+x ~/bin/repo
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/.local/bin/repo
+chmod a+x ~/.local/bin/repo
 
 sudo apt update && sudo apt upgrade
-sudo apt install -y bc bison build-essential ccache curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick lib32readline-dev lib32z1-dev libelf-dev liblz4-tool libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev llvm lib32ncurses5-dev libncurses5 libncurses5-dev python-is-python3
+sudo apt install -y bc bison build-essential ccache curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick lib32readline-dev lib32z1-dev libelf-dev liblz4-tool libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev llvm lib32ncurses5-dev libncurses5 libncurses5-dev python-is-python3 vim htop
 
-cat >>~/.bashrc<< EOF
-# add Android SDK platform tools to path
-if [ -d "$HOME/platform-tools" ] ; then
-    PATH="$HOME/platform-tools:$PATH"
-fi
-
-# set PATH so it includes user's private bin if it exists
-if [ -d "$HOME/bin" ] ; then
-    PATH="$HOME/bin:$PATH"
-fi
-
+cat >>~/.profile<< EOF
 export USE_CCACHE=1
 export CCACHE_EXEC=/usr/bin/ccache
 export CCACHE_DIR=/mnt/ccache
 EOF
+source ~/.profile
 
 mkdir -pv ~/bin
 mkdir -pv ~/android/lineage
@@ -42,4 +33,5 @@ repo sync -c --no-clone-bundle --no-tags -j"$(nproc --all)"
 repo sync -c --no-clone-bundle --no-tags -j"$(nproc --all)"
 repo sync -c --no-clone-bundle --no-tags -j"$(nproc --all)"
 
-echo "Now prepare Device Specific sourcecode and start building !"
+echo "INFO: Reboot to apply the environment variables Permanently."
+echo "INFO: Prepare device specific sourcecode and start building !"
